33528b577149615b3e716145a15774cbba999f5f
Irina Dovhgyalo <irinadovgyalo@gmail.com>
2019-07-24 16:11:32 +0300

    feat: add class Person

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-alert('hey!');
+function Person(name, age){
+	this.name = name;
+	let localAge = age;
+	this.getAge = function(){
+		return localAge;
+	}
+	this.setAge = function(age){
+		if(typeof age === 'number'){
+			localAge = age;
+		} else {
+			console.log('Enter a number, please!');
+		}
+	}
+}
+
+let mat = new Person('Mat', 16);
+console.log(mat.localAge); // undefined
+console.log(mat.name); // Mat
+console.log(mat.getAge()); // 16
+console.log(mat.setAge(25));
+console.log(mat.getAge()); // 25
