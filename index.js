b43e643502aa1084e6b292f339dcb3f1ab682b3a
Irina Dovhgyalo <irinadovgyalo@gmail.com>
2019-07-24 14:43:49 +0300

    feat: add datetime

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-alert('hey!');
+let currentDate = new Date();
+console.log(currentDate);
+
+console.log(currentDate.getDate());
+console.log(currentDate.getMonth());
+console.log(currentDate.getFullYear());
