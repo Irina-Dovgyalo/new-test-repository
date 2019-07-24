cda73eabd5c79bbbd9ee243f7e515e357d94b34b
Irina Dovhgyalo <irinadovgyalo@gmail.com>
2019-07-24 13:30:40 +0300

    feat: add object

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-alert('hey!');
+const user = {};
+user.name = 'Oleg';
+user.age = 20;
+
+console.log(user);
+
+user.age = 25;
+console.log(user);
+
+user.sum = function(a, b){
+    return a + b;
+}
+console.log(user);
+console.log(user.sum(5, 6));
